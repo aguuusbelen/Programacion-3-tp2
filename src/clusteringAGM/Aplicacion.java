4270af907859bac33abb9be1c58156e8c93dba4a
Agustina Mendez <agustinabmendez@gmail.com>
2022-09-26 13:49:54 -0300

    Update Aplicacion.java

--- src/clusteringAGM/Aplicacion.java
+++ src/clusteringAGM/Aplicacion.java
@@ -11,25 +11,30 @@ public class Aplicacion {
 	private HashMap<String, Point> listaVertices;
 
 	public Aplicacion() {
-		
+
 		listaVertices = new HashMap<>();
 	}
 
 	public void agregarVertice(String nombre, Integer posicionX, Integer posicionY) {
 		Point coordenada = new Point(posicionX, posicionY);
 		listaVertices.put(nombre, coordenada);
-		
+
 	}
 
 	public void crearGrafo() {
+		grafo = new Grafo(listaVertices.size());
+		// tengo que recorrer listaVertices y agregar cada coordenada (x,y)
+		// https://www.discoduroderoer.es/formas-de-recorrer-un-hashmap-en-java/
+
 		for (int i = 0; i < listaVertices.size(); ++i) {
-			
+			for (String clave : listaVertices.keySet()) {
+				grafo.agregarArista(listaVertices.get(clave));
+			}
+		}
 	}
 
 	public HashMap<String, Point> getListaVertices() {
 		return listaVertices;
 	}
-	
-	
 
 }
